fe532cfa5fd51beb079c323d698d7f4373a0bd81
Sourit Nandy <souritnandy123@gmail.com>
2025-07-11 20:15:47 +0530

    SignIn Function written successfully

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -8,14 +8,11 @@ class instaChat:
 
   def menu(self):
     user_input = input("""Welcome to our instaChat 👋  !! How would You like to proceed further 🙂?
-                       
                        1. Press 1 to signUp
                        2. Press 2 to signOut
                        3. Press 3 to write a post 
                        4. Press 4 to message a friend 
-                       5. Press 5 to exit
-                       Enter Your Choice: 
-                       """)
+                       5. Press 5 to exit""")
     if user_input == "1":
         self.signUp()
     elif user_input == "2":
@@ -30,8 +27,8 @@ class instaChat:
 
 
   def signUp(self):
-     email = input("Enter a valid email-address --> ")
-     password = input("Enter a strong password ---> ")
+     email = input("Enter a valid email-address:--> ")
+     password = input("Enter a strong password:---> ")
      self.username = email
      self.password = password
      print("🎉 --> Boom , You have been signed In succesfully")
